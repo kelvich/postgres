c91716da6053ccd8008585b0deeda79892885266
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-08-07 14:03:08 +0300

    Run WAL redo functions in a temporary memory context
    Some resource managers already used such a temporary memory context on
    their own. Let's consolidate that and run all WAL redo functions in a
    temporary context that is reset between each record.
    
    This leaves the rm_startup and rm_cleanup hooks unused in core code,
    but I kept them because they might still be used by extensions, and it
    might come handy for builtin rmgrs again in the future too.

--- src/backend/access/transam/xlogrecovery.c
+++ src/backend/access/transam/xlogrecovery.c
@@ -60,6 +60,7 @@
 #include "utils/datetime.h"
 #include "utils/fmgrprotos.h"
 #include "utils/guc_hooks.h"
+#include "utils/memutils.h"
 #include "utils/pg_lsn.h"
 #include "utils/ps_status.h"
 #include "utils/pg_rusage.h"
@@ -1727,6 +1728,8 @@ PerformWalRecovery(void)
 
 	if (record != NULL)
 	{
+		MemoryContext oldCtx;
+		MemoryContext redoCtx;
 		TimestampTz xtime;
 		PGRUsage	ru0;
 
@@ -1743,6 +1746,10 @@ PerformWalRecovery(void)
 		/* Prepare to report progress of the redo phase. */
 		if (!StandbyMode)
 			begin_startup_progress_phase();
+
+		redoCtx = AllocSetContextCreate(CurrentMemoryContext,
+										"recovery temporary context",
+										ALLOCSET_DEFAULT_SIZES);
 
 		/*
 		 * main redo apply loop
@@ -1819,7 +1826,10 @@ PerformWalRecovery(void)
 			/*
 			 * Apply the record
 			 */
+			oldCtx = MemoryContextSwitchTo(redoCtx);
 			ApplyWalRecord(xlogreader, record, &replayTLI);
+			MemoryContextSwitchTo(oldCtx);
+			MemoryContextReset(redoCtx);
 
 			/* Exit loop if we reached inclusive recovery target */
 			if (recoveryStopsAfter(xlogreader))
@@ -1835,6 +1845,8 @@ PerformWalRecovery(void)
 		/*
 		 * end of main redo apply loop
 		 */
+
+		MemoryContextSwitchTo(oldCtx);
 
 		if (reachedRecoveryTarget)
 		{
